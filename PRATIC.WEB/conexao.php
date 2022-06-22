8d050600ff80ced9ef27b87055a763a0b76e68ec
Sara Matos <106176445+SaraMatosSz@users.noreply.github.com>
2022-06-22 08:33:29 -0500

    Update conexao.php

--- PRATIC.WEB/conexao.php
+++ PRATIC.WEB/conexao.php
@@ -1,14 +1,12 @@
 <?php
-
-    $ servidor = " localhost " ;
-    $ servidor = "127.0.0.1" ;
-    $ bdname = "cadastro" ;
-    $ usuario = "raiz" ;
-    $ senha = "" ;
+    $servidor = "127.0.0.1";
+    $bdname = "cadastro";
+    $usuario = "root";
+    $senha = "";
  
-    $ conexao = mysqli_connect ( $ servidor , $ usuario , $ senha , $ bdname );
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $bdname);
 
-    if (! $ conexao )
-      die ( "Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error ());
-    echo  "Conexão realizada com sucesso!" ;
+    if(!$conexao)
+      die("Problemas com a conexão com o banco de dados. Descrição do problema: " . mysqli_connect_error());
+    echo "Conexão realizada com sucesso!";
 ?>
